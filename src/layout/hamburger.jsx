6e6a078ee0ae455612bda72ec1e967c525d3766d
Sadulla <alisherov.cs@gmail.com>
2026-06-03 13:14:38 +0500

    Update hamburger.jsx

--- src/layout/hamburger.jsx
+++ src/layout/hamburger.jsx
@@ -116,8 +116,8 @@ export default function HamburgerBar({ open, setOpen }) {
                   alt=""
                 />
               </div>
-              <button onClick={() => setOpen(false)} className="text-2xl text-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none" stroke="primary" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+              <button onClick={() => setOpen(false)} className="text-2xl">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
               </button>
             </div>
             <div className="overflow-y-auto">
